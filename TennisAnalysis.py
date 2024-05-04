2d698bd80beb6c475823abea8560999e6519e469
Li_Zizhen <e1075569@u.nus.edu>
2024-05-04 23:58:42 +0800

    Solved: Shot Tolerance, aces, double faults,

--- TennisAnalysis.py
+++ TennisAnalysis.py
@@ -47,19 +47,66 @@ def annotateWinLoseShot():
         if (player1Based.iloc[i, 4] == (player1Based.iloc[i-1,4]-3)):
             matchWithAnnotation.loc[i - 1, 'Winmark'] = 1
         if (player1Based.iloc[i, 4] == 3 and (i == matchLength - 1)):
-            matchWithAnnotation.loc[i - 1, 'Winmark'] = 1
-    for i in range(1, matchLength):
+            matchWithAnnotation.loc[i , 'Winmark'] = 1
         if (player1Based.iloc[i, 5] == (player1Based.iloc[i-1,5]+1)):
             matchWithAnnotation.loc[i-1, 'Winmark'] = -1
         if (player1Based.iloc[i, 5] == (player1Based.iloc[i-1,5]-3)):
             matchWithAnnotation.loc[i - 1, 'Winmark'] = -1
         if (player1Based.iloc[i, 5] == 3 and (i == matchLength - 1)):
-            matchWithAnnotation.loc[i - 1, 'Winmark'] = -1
+            matchWithAnnotation.loc[i , 'Winmark'] = -1
 
     return matchWithAnnotation
+
+# annotate length of points in player1's view, positive for player1 and negative for player2
+def annotateLength():
+    player1Based = swtichToPlayer1BasedTable()
+    matchWithAnnotation = match.assign(GameLength=0)
+    counter = 0
+
+    for i in range(1, matchLength):
+        if  (player1Based.iloc[i, 4] == (player1Based.iloc[i-1,4]+1)):
+            matchWithAnnotation.loc[i-1, 'GameLength'] = counter
+        elif  (player1Based.iloc[i, 4] == (player1Based.iloc[i-1,4]-3)):
+            matchWithAnnotation.loc[i-1, 'GameLength'] = counter
+        elif  (player1Based.iloc[i, 4] == 3 and (i == matchLength - 1)):
+            matchWithAnnotation.loc[i , 'GameLength'] = counter
+        elif  (player1Based.iloc[i, 5] == (player1Based.iloc[i-1,5]+1)):
+            matchWithAnnotation.loc[i-1, 'GameLength'] = -counter
+        elif  (player1Based.iloc[i, 5] == (player1Based.iloc[i-1,5]-3)):
+            matchWithAnnotation.loc[i-1, 'GameLength'] = -counter
+        elif  (player1Based.iloc[i, 5] == 3 and (i == matchLength - 1)):
+            matchWithAnnotation.loc[i , 'GameLength'] = -counter
+        else:
+            counter = counter + 1
+
+        if (player1Based.iloc[i, 12] == 1 or player1Based.iloc[i, 12] == 2):
+            counter = 1
+
+    return matchWithAnnotation
+
+# para: [start, end] player's winning chance if the length of point is between this range.
+# return: (winning chance, player's number of wins, opponent's number of wins)
+def getShotTolerance(start, end, playerIndex):
+    matchWithLength = annotateLength()
+    if (playerIndex == 1):
+        opponentNumberOfWin = sum((-end <= matchWithLength['GameLength']) & (matchWithLength['GameLength'] <= -start))
+        playerNumberOfWin = sum((matchWithLength['GameLength'] <= end) & (start <= matchWithLength['GameLength']))
+        return playerNumberOfWin, opponentNumberOfWin
+    if (playerIndex == 2):
+        playerNumberOfWin = sum((-end <= matchWithLength['GameLength']) & (matchWithLength['GameLength'] <= -start))
+        opponentNumberOfWin = sum((matchWithLength['GameLength'] <= end) & (start <= matchWithLength['GameLength']))
+        return playerNumberOfWin, opponentNumberOfWin
+
+
+# The index of successful first serve.
 def getSuccessfulFirstServeIndex(playerIndex):
     return (match.iloc[:, 0] == player[playerIndex - 1]["name"]) & (match.iloc[:,12] == 1) & (match.iloc[:,21] != 2)
 
+# The index of second serve
+def getSecondServeIndex(playerIndex):
+    return (match.iloc[:, 0] == player[playerIndex - 1]["name"]) & (match.iloc[:,12] == 2)
+
+# Represents what is the winning chance if the first serve is successful.
 def firstServeWinRate(playerIndex):
     numberOfWin = 0
     successfulFirstServeIndex = getSuccessfulFirstServeIndex(playerIndex)
@@ -75,6 +122,21 @@ def firstServeWinRate(playerIndex):
                 numberOfWin = numberOfWin + 1
     return numberOfWin / sum(successfulFirstServeIndex)
 
+# Represents what is the winning chance if has second serve (no mater second serve is fault or not)
+def secondServeWinRate(playerIndex):
+    numberOfWin = 0
+    secondServeIndex = getSecondServeIndex(playerIndex)
+    annotatedWithWinLose = annotateWinLoseShot()
+    for i in range(matchLength):
+        if(secondServeIndex[i] == True):
+            j = 0
+            while (annotatedWithWinLose.loc[i+j,'Winmark'] == 0):
+                j = j+1
+            if ((annotatedWithWinLose.loc[i+j,'Winmark'] == 1) and (playerIndex == 1)):
+                numberOfWin = numberOfWin + 1
+            if ((annotatedWithWinLose.loc[i+j,'Winmark'] == -1) and (playerIndex == 2)):
+                numberOfWin = numberOfWin + 1
+    return numberOfWin / sum(secondServeIndex),numberOfWin,  sum(secondServeIndex)
 
 
 #playerIndex = 1: first player, =2 second player
@@ -86,6 +148,12 @@ def getNumberOfServe(playerIndex):
     return sum((match.iloc[:, 0] == player[playerIndex - 1]["name"]) & (match.iloc[:,12] == 1))
 def getNumberOfSecondServe(playerIndex):
     return sum((match.iloc[:, 0] == player[playerIndex - 1]["name"]) & (match.iloc[:,12] == 2))
+
+# Get number of aces and double faults of a player, return (aces, double fault)
+def getNumberOfAcesAndDoubleFault(playerIndex):
+    return (sum((match.iloc[:, 0] == player[playerIndex - 1]["name"]) & (match.iloc[:,21] == 1)),
+            sum((match.iloc[:, 0] == player[playerIndex - 1]["name"]) & (match.iloc[:,21] == 2) & (match.iloc[:,12] == 2)))
+
 def storeToPlayer():
     player1["totalPointsWon"] = getPlayerTotalPointsWon(1)
     player2["totalPointsWon"] = getPlayerTotalPointsWon(2)
@@ -101,11 +169,18 @@ def storeToPlayer():
     player2["FirstServePercentage"] = 1 - player2["NumberOfSecondServe"]/player2["NumberOfServe"]
     player1["FirstServeWonRate"] = firstServeWinRate(1)
     player2["FirstServeWonRate"] = firstServeWinRate(2)
+    player1["SecondServeWonRate"] = secondServeWinRate(1)
+    player2["SecondServeWonRate"] = secondServeWinRate(2)
+    player1["AcesDoubleFault"] = getNumberOfAcesAndDoubleFault(1)
+    player2["AcesDoubleFault"] = getNumberOfAcesAndDoubleFault(2)
+    player1["ShotTolerance1-4"] = getShotTolerance(1,4,1)
+    player1["ShotTolerance5-8"] = getShotTolerance(5,8,1)
+    player1["ShotTolerance9-12"] = getShotTolerance(9,12,1)
+    player1["ShotTolerance13+"] = getShotTolerance(13,matchLength,1)
 
-
+# a = annotateLength()
+# print(a)
+# print(sum((a['GameLength']<5) & (a['GameLength']>0)))
+# print(sum((-5<a['GameLength']) & (a['GameLength']<0)))
 storeToPlayer()
-a = annotateWinLoseShot()
-b = getSuccessfulFirstServeIndex(1)
-c = firstServeWinRate(2)
-print(c)
 print(player1, player2)
